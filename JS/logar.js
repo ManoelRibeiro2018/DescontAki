162724ed10242d31c9fd651b5e5f6650b4c32c4c
manoel ribeiro <manoelribeiro20181@gmail.com>
2021-05-08 16:01:36 -0300

    Criação do modal na tela de login

---
+++ JS/logar.js
@@ -0,0 +1,81 @@
+
+
+const logar = document.querySelector('#logar');
+logar.addEventListener('click', () => {   
+
+    var cpf = document.getElementById("CPF").value
+    var senha = document.getElementById("senha").value 
+    localStorage.setItem('cpf', cpf)
+    // localStorage.setItem('nome', '') //tirei pq se n vai puxar do bd, n pode apagar
+    // localStorage.setItem('email', '')
+    // localStorage.setItem('sexo', '')
+    // localStorage.setItem('nascimento', '')
+    cpf = cpf.replace("-","").replace(".","").replace(".","")
+
+
+     
+    const modal = document.getElementById('modal-ativar');    
+
+    const validarConta = (data) =>  {
+        for (const atributos in data){
+            if (data["cpf"] == cpf && data["senha"] == senha){
+                window.location.href = "home.html"
+            } 
+            else if (modal)
+            {
+
+                document.getElementById("msgErro").innerHTML = "Os dados informados estão incorretos!"
+                console.log(document.getElementById("msgErro").innerHtml)
+                modal.classList.add('mostrar');
+
+                modal.addEventListener('click', (e) => {
+
+                    if (e.target.id == 'modal-ativar') {
+                        modal.classList.remove('mostrar');
+                      
+                    }         
+                })
+              }
+    
+            }
+    }
+
+    //fetch pegando os valores do banco
+    fetch(`http://pblelcoma-final.herokuapp.com/usuarios/cpf/${cpf}`,
+        {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer'            
+        })
+        .then(response => {
+            if (!response.ok)
+            {
+                throw Error(response.statusText); 
+            }
+
+            response.json().then(data => validarConta(data))
+
+        }).catch(function(error){
+
+            if (modal)
+            { 
+                document.getElementById("msgErro").innerHTML = "CPF ou senha inválidos, por favor tente novamente!";
+                   
+                console.log(document.getElementById("msgErro").innerHtml)
+                modal.classList.add('mostrar');
+
+                modal.addEventListener('click', (e) => {
+
+                    if (e.target.id == 'modal-ativar') {
+                        modal.classList.remove('mostrar'); 
+                         
+                    }         
+                })
+              }
+        })
+
+    
+ })
