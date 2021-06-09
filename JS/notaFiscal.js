cb36a567903e36646ab6a79001ee899b1bc52694
DIR\jully.nixon <74429270+jully-nixon@users.noreply.github.com>
2021-06-09 15:04:46 -0300

    Nome do lojista da nota cadastrada sendo mostrado no toast.

--- JS/notaFiscal.js
+++ JS/notaFiscal.js
@@ -1,21 +1,22 @@
+let lojista = '';
+
 function startToast() {
-    const lojista = 'Americanas';
     Toastify({
         text: `Cupom ${lojista} cadastrado com sucesso!`,
         duration: 3000,
-        gravity: "top", 
-        position: "right", 
+        gravity: "top",
+        position: "right",
         backgroundColor: "rgb(10 187 10)",
-        stopOnFocus: true, 
+        stopOnFocus: true,
         close: true,
         className: "showToast"
-      }).showToast();
+    }).showToast();
 }
 function dismissLoad() {
     setTimeout(function () {
         document.getElementById("loading").style.display = 'none';
     }, 2000);
-} 
+}
 function showNewInvoice() {
     document.getElementById('newInvoice').style.display = "block";
     document.getElementById('preview').style.display = "none";
@@ -23,4 +24,34 @@ function showNewInvoice() {
 function hiddenNewInvoice() {
     document.getElementById('newInvoice').style.display = "none";
     document.getElementById('preview').style.display = "block";
+}
+function InserindoNota(content) {
+    var codigoUsuairo = localStorage.getItem("CodSession")
+    var valores = {
+        url: content,
+        "usuario": {
+            "id": codigoUsuairo
+        }
+    };
+    fetch("https://pblelcoma-final.herokuapp.com/notafiscal",
+        {
+            method: 'post',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(valores),
+
+        })
+
+        .then((res) => res.json())
+        .then((result) => {
+            lojista = result.nome;
+        }).catch(function (error) {
+            console.error(error);
+        })
 }
